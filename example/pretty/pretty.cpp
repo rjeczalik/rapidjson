c456e440b6b0b4f7d1144e0c7713a997588aed7c
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-27 02:58:16 +0400

    Examples: fix style

--- example/pretty/pretty.cpp
+++ example/pretty/pretty.cpp
@@ -1,5 +1,6 @@
 // JSON pretty formatting example
-// This example can only handle UTF-8. For handling other encodings, see prettyauto example.
+// This example can only handle UTF-8. For handling other encodings,
+// see prettyauto example.
 
 #include "rapidjson/reader.h"
 #include "rapidjson/prettywriter.h"
@@ -8,22 +9,29 @@
 
 using namespace rapidjson;
 
-int main(int, char*[]) {
-	// Prepare reader and input stream.
-	Reader reader;
-	char readBuffer[65536];
-	FileReadStream is(stdin, readBuffer, sizeof(readBuffer));
+int main() {
+  fprintf(stdout, "Enter json string (hit ctrl-D at end):\n");
 
-	// Prepare writer and output stream.
-	char writeBuffer[65536];
-	FileWriteStream os(stdout, writeBuffer, sizeof(writeBuffer));
-	PrettyWriter<FileWriteStream> writer(os);
+  // Prepare reader and input stream.
+  Reader reader;
+  char readBuffer[65536];
+  FileReadStream is(stdin, readBuffer, sizeof(readBuffer));
 
-	// JSON reader parse from the input stream and let writer generate the output.
-	if (!reader.Parse<kParseValidateEncodingFlag>(is, writer)) {
-		fprintf(stderr, "\nError(%u): %s\n", (unsigned)reader.GetErrorOffset(), reader.GetParseError());
-		return 1;
-	}
+  // Prepare writer and output stream.
+  char writeBuffer[65536];
+  FileWriteStream os(stdout, writeBuffer, sizeof(writeBuffer));
+  PrettyWriter<FileWriteStream> writer(os);
 
-	return 0;
+  // JSON reader parse from the input stream and let writer generate the output.
+  if (!reader.Parse<kParseValidateEncodingFlag>(is, writer)) {
+    fprintf(
+        stderr,
+        "\nError(%u): %s\n",
+        (unsigned)reader.GetErrorOffset(),
+        reader.GetParseError()
+    );
+    return 1;
+  }
+
+  return 0;
 }
